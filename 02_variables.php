252249bb1c7387dac94a5eaf4975e9775978ce83
Adindahutasoit0204 <124247757+Adindahutasoit0204@users.noreply.github.com>
2023-02-02 16:43:39 +0700

    Add files via upload

---
+++ 02_variables.php
@@ -0,0 +1,21 @@
+<?<php 
+    $Tipe_Mobil = "sport";
+    $Nama_Mobil = "Toyota 86";
+    $Persediaan = 2;
+    $Harga = 775750000;
+    $dealer = "KitaAja";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Showroom Jual Mobil dan Motor KitaAja</title>
+</head>
+<body><b>Daftar Mobil dan Motor Dealer KitaAja Jika Tidak ada Diskon </b><br>
+    <?php
+    echo ("Tipe Mobil = ".$Tipe_Mobil. '<br>');
+    echo ("Nama Mobil = ".$Nama_Mobil. '<br>');
+    echo ("Persediaan = ".$Persediaan. '<br>');
+    ?>
+</body>
+</html>
